f616495295e60f535282dad47869d212c24fd3de
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-06-26 15:23:29 +0200

    yes

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -365,6 +365,11 @@ const events = function (isInitialSetup, isStage, targetId, colors) {
             <block type="event_whenstageclicked"/>
         ` : `
             <block type="event_whenthisspriteclicked"/>
+            <block type="event_whentouchingobject">
+                <value name="TOUCHINGOBJECTMENU">
+                    <shadow type="sensing_touchingobjectmenu"/>
+                </value>
+            </block>
         `}
         <block type="event_whenbackdropswitchesto">
         </block>
